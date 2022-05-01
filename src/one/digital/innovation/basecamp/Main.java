ad77b7566e52171b6f2c28ed74aeaa553e0f3486
Ana Clara <gomesclara99x@gmail.com>
2022-05-01 15:48:15 -0300

    first commit

---
+++ src/one/digital/innovation/basecamp/Main.java
@@ -0,0 +1,48 @@
+package one.digital.innovation.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+        //os comentários são formas erradas ou que não fazem parte das boas práticas de se declarar váriaveis em java
+       int i;
+       //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i= 5;
+        I= 10;
+        _1a= 20;
+        $aq= 7;
+
+        final int j = 10;
+        //j = 15;
+        int arsn24678md;
+        int arsn24678_md;
+        //int arsn24678%md;
+
+        arsn24678md = 100;
+        arsn24678_md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(arsn24678md);
+        System.out.println(arsn24678_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+}
